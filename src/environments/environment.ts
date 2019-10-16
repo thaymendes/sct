3ec95638f3dad3e2ca0702f1f075f319955e4c16
Mendes, Thayanne Viana <thay489@gmail.com>
2019-10-15 22:52:39 -0300

    Configurações Iniciais

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+	apiKey: "AIzaSyCc2ei8WzEAdG_8RfRNGm4q6a69AyZRoeQ",
+    authDomain: "sct-db.firebaseapp.com",
+    databaseURL: "https://sct-db.firebaseio.com",
+    projectId: "sct-db",
+    storageBucket: "sct-db.appspot.com",
+    messagingSenderId: "208954300613",
+    appId: "1:208954300613:web:46f0f08a4f53dc3d0ef8ac",
+    measurementId: "G-Z156BJ9W92"
+	}
 };
 
 /*
